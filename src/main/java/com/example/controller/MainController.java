4b7b743adde7c0459567ecbe6c87f721d28ac02c
ehddnkim <rlaehddn949@gmail.com>
2025-05-16 16:05:08 +0900

    업데이트

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -2,8 +2,11 @@ package com.example.controller;
 
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,11 @@ public class MainController {
 	@PostMapping(value = "/write")
 	public Map<String, Object> write() {
 		return null;
+	}
+	
+	@PostMapping(value = "/update")
+	public ResponseEntity<Resource> update(){
+		return "";
 	}
 	
 }
