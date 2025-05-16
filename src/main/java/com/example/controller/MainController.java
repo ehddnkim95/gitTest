5479c1af3fba9e972ca9c31622647cad49b7df77
ehddnkim <rlaehddn949@gmail.com>
2025-05-16 15:59:18 +0900

    추가

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +24,10 @@ public class MainController {
 	public String list() {
 		return "";
 	}
+	
+	@PostMapping(value = "/write")
+	public Map<String, Object> write() {
+		return null;
+	}
+	
 }
