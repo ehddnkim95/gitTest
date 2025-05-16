75eb7859a8023995e501c40e4b073d05d0e8a9f4
ehddnkim <rlaehddn949@gmail.com>
2025-05-16 15:37:01 +0900

    이름

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,16 +1,20 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
 	@GetMapping(value = "/")
 	public String main() {
 		
-		System.out.println("Hello");
-		
+		logger.info("Hello ehddnkim");
 		return "{\"msg\":\"OK\"}";
 	}
 }
