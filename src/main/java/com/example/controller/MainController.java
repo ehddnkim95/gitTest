1b99f7102b676ef3f4340f8ca666a3857a8b4448
ehddnkim <rlaehddn949@gmail.com>
2025-05-16 16:10:43 +0900

    삭제추가

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,13 @@ public class MainController {
 	
 	@PostMapping(value = "/update")
 	public ResponseEntity<Resource> update(){
-		return "";
+		return null;
 	}
+	
+	@DeleteMapping(value = "/del")
+	public ResponseEntity<?> del(){
+		return null;
+	}
+	
 	
 }
