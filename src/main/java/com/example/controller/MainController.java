01aba89db4d92a6cbe19a4bc818f1a35fca5b4f4
ehddnkim <rlaehddn949@gmail.com>
2025-05-16 15:19:56 +0900

    이름 추가

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
 
+	@GetMapping(value = "/")
+	public String main() {
+		
+		System.out.println("Hello");
+		
+		return "{\"msg\":\"OK\"}";
+	}
 }
